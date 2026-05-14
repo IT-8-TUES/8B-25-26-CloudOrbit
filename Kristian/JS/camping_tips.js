f379d31d0f7ee89d1a70082cbf5d0aaf3c17e096
KrisAtanasov1 <kristianatanasov2011@gmail.com>
2026-05-14 14:56:12 +0300

    Samo Dobavqne na Komentari

--- Kristian/JS/camping_tips.js
+++ Kristian/JS/camping_tips.js
@@ -1,11 +1,14 @@
+// Get references to the menu button, close button, and mobile menu elements
 const menuBtn = document.getElementById("menuBtn");
 const closeBtn = document.getElementById("closeBtn");
 const mobileMenu = document.getElementById("mobileMenu");
 
+// Open the mobile menu when the hamburger menu button is clicked
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.add("show");
 });
 
+// Close the mobile menu when the close button (X) is clicked
 closeBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("show");
 });
